f9a1e54deb4994d3d10369e64c228e1ca2268197
Naveen12345-alt <naveenchauhan1234@protonmail.com>
2021-05-30 12:35:57 +0530

    Nugget:1-->Multiply_Arguments-2

--- Nuggets/Multiply_Arguments.js
+++ Nuggets/Multiply_Arguments.js
@@ -1,9 +1,17 @@
+/**
+ * arguments inside function collects all arguments passed to a function
+ * but it's arraylike and not truely array, so to make it array, use spread
+ * operator
+ */
 function multiply1() {
     const args = [...arguments]
 
     console.log(args.reduce((acc, item) => (item ? item * acc : item + acc), 1))
 }
 
+/**
+ * we can use rest operator to grab all arguments and pass them to an array.
+ */
 function multiply2(...args) {
     console.log(args.reduce((acc, item) => (item ? item * acc : item + acc), 1))
 }
