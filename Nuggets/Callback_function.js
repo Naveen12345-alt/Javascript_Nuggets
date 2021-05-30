baf49125479da912bdb0283c6b910132baf4d661
Naveen12345-alt <naveenchauhan1234@protonmail.com>
2021-05-30 12:06:26 +0530

    Nugget:2>Callback_function

---
+++ Nuggets/Callback_function.js
@@ -0,0 +1,32 @@
+/**
+ * Callback functions are functions that we pass in as arguments
+ * and execute it later
+ */
+
+function makeUpperCase(value) {
+    console.log(value.toUpperCase())
+}
+
+/**
+ * cb is our callback function
+ */
+function handleName(name, cb) {
+    const fullName = `${name} chauhan`
+
+    cb(fullName)
+}
+
+/**
+ * makeUpperCase is just referenced here,
+ * if we had done something like makeUpperCase(), then we would
+ * have called the function thereitself, which is not we want
+ */
+handleName('Naveen', makeUpperCase)
+handleName('Naveen', (value) => console.log(value))
+
+/**
+ * usage:>
+ * array methods (eg: map, filter, etc)
+ * setTimeout, setInterval
+ * event listeners
+ */
